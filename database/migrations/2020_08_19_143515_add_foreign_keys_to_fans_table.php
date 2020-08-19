3bc3c17bfdb96636d2c58ac023411a2351240aa2
xuan <wdftmmas234@gmail.com>
2020-08-19 22:52:33 +0800

    新增 fans table

---
+++ database/migrations/2020_08_19_143515_add_foreign_keys_to_fans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToFansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fans', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->foreign('fan_id')->references('id')->on('users')->onDelete('cascade');;
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fans', function (Blueprint $table) {
+            //
+
+            $table->dropForeign('user_id');
+            $table->dropForeign('fan_id');
+        });
+    }
+}
